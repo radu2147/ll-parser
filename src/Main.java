d76f8bb42475263152656196f4dcc871690d1c6c
RADU <radubaston1@gmail.com>
2021-12-12 13:54:59 +0200

    Added parsing table construction

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 import parser.Grammar;
+import parser.LLParser;
 
 public class Main {
     public static void main(String[] args) {
         var grm = new Grammar();
         grm.readGrammarFromFile("C:\\Users\\RADU\\Desktop\\my_projects\\facultate\\LLParser\\test1.txt");
-        grm.follow("A").forEach(System.out::println);
+        var parser = new LLParser(grm);
+        try {
+            parser.constructTable();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.println("DONE");
+
     }
 }
--- src/parser/LLParser.java
+++ src/parser/LLParser.java
@@ -1,5 +1,44 @@
 package parser;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LLParser {
+
+    Grammar grm;
+    Map<String, Map<String,ProdRule>> table;
+
+    public LLParser(Grammar grm){
+        this.grm = grm;
+        this.table = new HashMap<>();
+        for(var key: this.grm.prodRules.keySet()){
+            table.put(key, new HashMap<>());
+        }
+    }
+
+    public void constructTable() throws Exception {
+        for(var el: this.grm.prodRules.keySet()){
+            for(var rule: this.grm.prodRules.get(el)){
+                var first = grm.first(String.valueOf(rule.right.charAt(0)));
+                for(var terminal: first){
+                    if(!terminal.equals(Grammar.EPSILON)){
+                        if(table.get(rule.left).containsKey(terminal)){
+                            throw new Exception("Grammar is not LL");
+                        }
+                        table.get(rule.left).put(terminal, rule);
+                    }
+                    else{
+                        var follow = grm.follow(rule.left);
+                        for(var fl: follow){
+                            if(table.get(rule.left).containsKey(fl)){
+                                throw new Exception("Grammar is not LL");
+                            }
+                            table.get(rule.left).put(fl, rule);
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 }
